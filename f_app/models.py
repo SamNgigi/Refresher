0b11191af4562bb389fea8e7d37d6460bab58fe8
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-10-08 10:19:30 +0300

    Many to Many implementation on User working. Going deeper

--- f_app/models.py
+++ f_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Publication(models.Model):
+  title = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.title
+
+  class Meta:
+    ordering = ('title',)
+
+class Article(models.Model):
+  headline = models.CharField(max_length=100)
+  publication = models.ManyToManyField(Publication)
+
+  def __str__(self):
+    return self.headline
+
+  class Meta:
+    ordering = ('headline',)
+
+class User(models.Model):
+  name = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    ordering = ('name',)
+
+class Post(models.Model):
+  content = models.CharField(max_length=100)
+  likers = models.ManyToManyField(User)
+
+  def __str__(self):
+    return self.content
+
+  class Meta:
+    ordering = ('content',)
+
+class Friendsters(models.Model):
+  user = models.OneToOneField(User, related_name='profile')
+  follow = models.ManyToManyField(User, related_name=followed_by)
